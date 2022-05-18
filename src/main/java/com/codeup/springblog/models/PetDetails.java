e17bcf28c43bc5706a2d2d70d3d4f8ce057ac6d3
Christopher Berry <christopher.m.berry01@gmail.com>
2022-05-18 11:45:48 -0500

    modify petDetails

--- src/main/java/com/codeup/springblog/models/PetDetails.java
+++ src/main/java/com/codeup/springblog/models/PetDetails.java
@@ -15,4 +15,51 @@ public class PetDetails {
     private boolean isSterile;
 
     private int weightInOunces;
+
+    public PetDetails(){}
+
+    public PetDetails(long id, String personalityDescription, boolean isSterile, int weightInOunces) {
+        this.id = id;
+        this.personalityDescription = personalityDescription;
+        this.isSterile = isSterile;
+        this.weightInOunces = weightInOunces;
+    }
+
+    public PetDetails(String personalityDescription, boolean isSterile, int weightInOunces) {
+        this.personalityDescription = personalityDescription;
+        this.isSterile = isSterile;
+        this.weightInOunces = weightInOunces;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPersonalityDescription() {
+        return personalityDescription;
+    }
+
+    public void setPersonalityDescription(String personalityDescription) {
+        this.personalityDescription = personalityDescription;
+    }
+
+    public boolean isSterile() {
+        return isSterile;
+    }
+
+    public void setSterile(boolean sterile) {
+        isSterile = sterile;
+    }
+
+    public int getWeightInOunces() {
+        return weightInOunces;
+    }
+
+    public void setWeightInOunces(int weightInOunces) {
+        this.weightInOunces = weightInOunces;
+    }
 }
