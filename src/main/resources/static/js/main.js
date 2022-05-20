fee40c377da278acac8182f5529b244744ac431b
Christopher Berry <christopher.m.berry01@gmail.com>
2022-05-20 11:04:05 -0500

    add validation upon edit form submission

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,15 @@
-
+// redirect view
 $(".edit").on("click", function (){
     let postId = $(this).attr("data-id")
     window.location = "/posts/" + postId
 });
 
+// form validation
+$('#formId').submit(function (e) {
+    if ($('#tag').val() == '') {
+        alert("Please select an option");
+        e.preventDefault(); //stop form submission
+    } else {
+        return true;
+    }
+});
